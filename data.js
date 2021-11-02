a5b7d47754cad1663750fd6dd2443e841550faac
eugene44-hub <eugeneishado44@gmail.com>
2021-11-02 14:07:08 +0100

    fixed lat and lon

--- data.js
+++ data.js
@@ -38,20 +38,60 @@ go.addEventListener('click', e => {
             // console.log(data.json.lat)
             // console.log(data.json.lon)
             console.log(data)
+
             mapboxgl.accessToken = 'pk.eyJ1IjoiZXVnZW5pY3MiLCJhIjoiY2t2MGM0b3N1MDFqdDJvbHNkcjlrZTNlMSJ9.fK6MzVndal6J3DSmNxsFsw';
-            var map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [lon, lat],
-                zoom: 13
+            const map = new mapboxgl.Map({
+                container: 'map', // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: [lon, lat], // starting position
+                zoom: 9 // starting zoom
             });
+
+            map.addControl(new mapboxgl.NavigationControl());
+            map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body') }));
+
+            const marker = new mapboxgl.Marker({
+                    color: "red",
+                    draggable: true
+                }).setLngLat([lon, lat])
+                .addTo(map);
+
+
         })
 })
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXVnZW5pY3MiLCJhIjoiY2t2MGM0b3N1MDFqdDJvbHNkcjlrZTNlMSJ9.fK6MzVndal6J3DSmNxsFsw';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [3.328670, 6.452560],
-    zoom: 10
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position
+    zoom: 9 // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body') }));
+
+
+// Set marker options.
+
+// Create a new marker.
+
+// map.addControl(
+//     new MapboxGeocoder({
+//         accessToken: mapboxgl.accessToken,
+//         localGeocoder: coordinatesGeocoder,
+//         zoom: 4,
+//         placeholder: 'Try: -40, 170',
+//         mapboxgl: mapboxgl,
+//         reverseGeocode: true
+//     })
+// );
+
+// map.touchZoomRotate.enable()
+// map.scrollZoom.enable();
+// map.boxZoom.enable();
+// map.dragRotate.enable();
+// map.dragPan.enable();
+// map.keyboard.enable();
+// map.doubleClickZoom.enable();
+// map.dragPan.enable()
